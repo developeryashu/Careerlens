c444f49aa371e20a5d7d0d2392e9541f699c41cd
developeryashu <b250572@skit.ac.in>
2026-02-07 17:05:31 +0530

    CORRETIONS

--- app/auth/sign-up-success/page.tsx
+++ app/auth/sign-up-success/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Mail } from "lucide-react"
-import "./page.css"
 
 export default function SignUpSuccessPage() {
   return (
